b6b4baa6aff0d49819961637f962254aad5f6305
Wolfrum5150 <nicholas.e.saylor@gmail.com>
2024-01-24 13:52:46 -0500

    Added artist page link constructor

--- musicwordcloud.py
+++ musicwordcloud.py
@@ -8,6 +8,7 @@ HTML_TAG_RE = re.compile(r'(\<[^>]*\>)+')
 
 
 def main():
+    artist_song_page = build_artist_page(input("Enter artist name:"))
     #TODO: Find url dynamically
     #url = 'https://genius.com/Caligulas-horse-the-world-breathes-with-me-lyrics'
     #url = 'https://genius.com/Nwa-fuck-tha-police-lyrics'
@@ -19,10 +20,16 @@ def main():
         print(remove_fluff(item.decode_contents()))
 
 
-def remove_fluff(element):
+def remove_fluff(element) -> str:
     """Removes html tags, section names, and additional non-lyric text from lyrics"""
     element = re.sub(HTML_TAG_RE, ' ', element)
     return re.sub(SECTION_RE, '', element)
+
+
+def build_artist_page(artist: str) -> str:
+    base_url = "https://genius.com/artists/"
+    constructed_url = re.sub(r'[^a-zA-Z0-9-]', '', artist.replace(" ", "-").lower())
+    return base_url + constructed_url + "/songs"
 
 
 if __name__ == "__main__":
