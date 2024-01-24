dff9d3b64e66c32d71e1d0cbae74a4354c9b87b6
Wolfrum5150 <nicholas.e.saylor@gmail.com>
2024-01-24 13:30:18 -0500

    Fixed issue with html tags causing undesired formatting

--- musicwordcloud.py
+++ musicwordcloud.py
@@ -3,21 +3,25 @@ import requests
 import re
 
 
-section_re = re.compile(r'\[.*\]')
+section_re = re.compile(r'\[[^\[\]]*\]')
+html_tag_re = re.compile(r'(\<[^>]*\>)+')
 
 
 def main():
     #TODO: Find url dynamically
-    url = 'https://genius.com/Caligulas-horse-the-world-breathes-with-me-lyrics'
+    #url = 'https://genius.com/Caligulas-horse-the-world-breathes-with-me-lyrics'
+    #url = 'https://genius.com/Nwa-fuck-tha-police-lyrics'
+    url = 'https://genius.com/Eminem-lose-yourself-lyrics'
     response = requests.get(url)
     soup = bs(response.text, 'html.parser')
-    #TODO: Confirm class is consistent between pages
     lyrics_elements = soup.find_all('div', class_='Lyrics__Container-sc-1ynbvzw-1 kUgSbL')
     for item in lyrics_elements:
-        print(remove_sections(item.get_text(separator='\n', strip=True)))
+        print(remove_fluff(item.decode_contents()))
 
 
-def remove_sections(element):
+def remove_fluff(element):
+    """Removes html tags, section names, and additional non-lyric text from lyrics"""
+    element = re.sub(html_tag_re, ' ', element)
     return re.sub(section_re, '', element)
 
 
