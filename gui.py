c833b9f70a7504b6ba55a5299e9b4bf4ba7ed2b2
Nicholas Saylor <nicholas.e.saylor@gmail.com>
2024-06-18 03:33:43 -0400

    Use proper method of getting widgets

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@ from PIL import ImageTk, Image
 from pathvalidate import sanitize_filename
 from multiprocessing import freeze_support
 import tkinter as tk
+from copy import deepcopy
 from tkinter import ttk, messagebox, filedialog
 from ttkthemes import ThemedTk
 import sys
@@ -11,11 +12,7 @@ import threading
 
 current_cloud: WordCloud
 next_cloud: WordCloud
-cloud_frame: tk.Frame
-submit_button: tk.Button
-save_button: tk.Button
 thread: threading.Thread
-root: tk.Tk
 
 
 class TextRedirector:
@@ -53,22 +50,28 @@ def check_thread():
     or gives error for invalid artist
     """
     if thread.is_alive():
-        root.after(100, check_thread)
+        window.after(100, check_thread)
     else:
         global current_cloud
         if next_cloud:
             current_cloud = next_cloud
             display_cloud()
-            save_button["state"] = tk.NORMAL
+            window.nametowidget("entry_frame_wrapper.entry_frame.save_button")[
+                "state"
+            ] = tk.NORMAL
         else:
             messagebox.showerror(
                 "Could not find artist",
                 "Artist could not be found on Genius, ensure that it is spelled correctly.",
             )
-        submit_button["state"] = tk.NORMAL
+        window.nametowidget("entry_frame_wrapper.entry_frame.submit_button")[
+            "state"
+        ] = tk.NORMAL
 
 
 def save_cloud(artist: str):
+    # Avoid race conditions by copying current cloud immediately
+    cloud_copy = deepcopy(current_cloud)
     try:
         file_path = filedialog.asksaveasfilename(
             defaultextension=".png",
@@ -84,7 +87,7 @@ def save_cloud(artist: str):
         )
         if file_path:
             print(f"Saving file to {file_path}")
-            current_cloud.to_file(file_path)
+            cloud_copy.to_file(file_path)
             print("Successfully saved file!")
         else:
             print("Cancelled saving file")
@@ -100,8 +103,10 @@ def get_cloud(artist: str):
     global thread
     thread = threading.Thread(target=threaded_generation, args=(artist,))
     thread.start()
-    submit_button["state"] = tk.DISABLED
-    root.after(1000, check_thread)
+    window.nametowidget("entry_frame_wrapper.entry_frame.submit_button")["state"] = (
+        tk.DISABLED
+    )
+    window.after(1000, check_thread)
 
 
 def display_cloud(event=None):
@@ -113,6 +118,7 @@ def display_cloud(event=None):
     if current_cloud is not None:
         wc_image = current_cloud.to_array()
         pil_image = Image.fromarray(wc_image)
+        cloud_frame = window.nametowidget("cloud_frame")
         size = min(cloud_frame.winfo_width(), cloud_frame.winfo_height())
         pil_image = pil_image.resize((size, size), Image.LANCZOS)
         tk_image = ImageTk.PhotoImage(pil_image)
@@ -131,7 +137,6 @@ def set_up_gui() -> tk.Tk:
     """
     # Prevent error on start-up
     global current_cloud
-    global root
     current_cloud = None
     # Create window
     root = ThemedTk(theme="equilux")
@@ -140,25 +145,28 @@ def set_up_gui() -> tk.Tk:
     # Create Frames
     text_frame = ttk.Frame(root)
     text_frame.pack(side=tk.BOTTOM, fill=tk.X)
-    entry_frame_wrapper = ttk.Frame(root)
+    entry_frame_wrapper = ttk.Frame(root, name="entry_frame_wrapper")
     entry_frame_wrapper.pack(side=tk.BOTTOM, fill=tk.X)
-    entry_frame = ttk.Frame(entry_frame_wrapper)
+    entry_frame = ttk.Frame(entry_frame_wrapper, name="entry_frame")
     entry_frame.pack(side=tk.BOTTOM, anchor=tk.CENTER)
-    global cloud_frame
-    cloud_frame = ttk.Frame(root)
+    cloud_frame = ttk.Frame(root, name="cloud_frame")
     cloud_frame.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True, pady=(0, 5), padx=5)
     # For changing size of cloud
     cloud_frame.bind("<Configure>", display_cloud)
     # Create content
     artist_entry_label = ttk.Label(entry_frame, text="Enter an artist:")
     artist_entry = ttk.Entry(entry_frame, width=30)
-    global submit_button
     submit_button = ttk.Button(
-        entry_frame, text="Submit", command=lambda: get_cloud(artist_entry.get())
+        entry_frame,
+        text="Submit",
+        command=lambda: get_cloud(artist_entry.get()),
+        name="submit_button",
     )
-    global save_button
     save_button = ttk.Button(
-        entry_frame, text="Save as...", command=lambda: save_cloud(artist_entry.get())
+        entry_frame,
+        text="Save as...",
+        command=lambda: save_cloud(artist_entry.get()),
+        name="save_button",
     )
     save_button["state"] = tk.DISABLED
     text_output = tk.Text(text_frame, wrap=tk.WORD, height=6, width=75)
@@ -175,5 +183,5 @@ def set_up_gui() -> tk.Tk:
 
 if __name__ == "__main__":
     freeze_support()
-    root = set_up_gui()
-    root.mainloop()
+    window = set_up_gui()
+    window.mainloop()
