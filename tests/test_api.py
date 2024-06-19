a70c486420ebb1e8652fed1723737c8e7bd82be0
Nicholas Saylor <nicholas.e.saylor@gmail.com>
2024-06-19 01:20:13 -0400

    Make tests more explicit

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,13 +7,13 @@ class TestApi(unittest.TestCase):
         """Test various artists with simple names"""
         for name in ["Seether", "Taylor Swift", "Sza", "Metallica", "Charli XCX", "TOMORROW X TOGETHER"]:
             with self.subTest(name=name):
-                self.assertNotEqual(find_api(build_artist_page(name), name), "")
+                self.assertNotEqual(find_api(build_artist_page(name), name), "", f"{name} failed.")
 
     def test_api_robust(self):
         """Tests various artists with complex names"""
         for name in ["AC/DC", "J. Cole", "Beyoncé", "Björk", "D'Angelo"]:
             with self.subTest(name=name):
-                self.assertNotEqual(find_api(build_artist_page(name), name), "")
+                self.assertNotEqual(find_api(build_artist_page(name), name), "", f"{name} failed.")
 
     def test_not_implemented(self):
         """Test artists with names in scripts not supported"""
@@ -21,20 +21,20 @@ class TestApi(unittest.TestCase):
         # Romanized Names are Camellia, Jay Chou, Epik High,
         for name in ["かめりあ", "周杰倫", "에픽하이"]:
             with self.subTest(name=name):
-                self.assertEqual(find_api(build_artist_page(name), name), "")
+                self.assertEqual(find_api(build_artist_page(name), name), "", f"{name} failed.")
 
     def test_api_fails(self):
         """Tests names where the API call should fail"""
         for name in ["", "/", "asddasnopdas", ";"]:
             with self.subTest(name=name):
-                self.assertEqual(find_api(build_artist_page(name), name), "")
+                self.assertEqual(find_api(build_artist_page(name), name), "", f"{name} failed.")
 
     def test_api_edge_cases(self):
         """Following tests should pass despite looking like it should fail"""
         # These succeed due to unidecode being run on the strings
         for name in [";return", ";return;", "return;", "?return"]:
             with self.subTest(name=name):
-                self.assertNotEqual(find_api(build_artist_page(name), name), "")
+                self.assertNotEqual(find_api(build_artist_page(name), name), "", f"{name} failed.")
 
 
 if __name__ == '__main__':
