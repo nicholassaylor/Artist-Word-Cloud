8a17db0e3cd0fe4da563c1b532fec5e885099ce3
Nicholas Saylor <79181893+nicholassaylor@users.noreply.github.com>
2024-06-20 15:28:46 -0400

    Refactor Core
    * Separates CLI program from core functionality
    * Renames and refactors various files and variables for readability
    * User-facing programs are separate from cloud building package
    * Other minor refactors and reformats

---
+++ artistwordcloud/cloud_creation.py
@@ -0,0 +1,124 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+from multiprocessing import Pool
+from unidecode import unidecode
+from wordcloud import WordCloud
+
+from artistwordcloud.constants import (
+    ARTIST_RE,
+    CLEAN_LYRICS_RE,
+    COMBINED_STOPWORDS,
+    LYRIC_CLASS,
+)
+
+
+def build_artist_page(artist_name: str) -> str:
+    """
+    Returns a link to an artist's Genius page when given their name
+    """
+    base_url = "https://genius.com/artists/"
+    # Non-alphanumeric characters are excluded from Genius links, they are effectively replaced with ''
+    # Spaces are replaced with '-'
+    constructed_url = re.sub(ARTIST_RE, "", artist_name.replace(" ", "-").lower())
+    return base_url + constructed_url + "/songs"
+
+
+def build_song_links(artist_page: str, artist_name: str) -> list:
+    """
+    Compiles a list of song links associated to a particular artist
+    Pulls data from Genius's API
+    """
+    api_string = find_api(artist_page, artist_name)
+    if api_string == "":
+        raise ValueError()
+    print(
+        "Collecting links...\nDepending on the size of the artist's library, this may take a while..."
+    )
+    content = requests.get(
+        f"https://genius.com/api/artists/{api_string}/songs?page=1&per_page=20&sort=popularity&text_format=html"
+    ).json()
+    link_list = []
+    while True:
+        for entry in content["response"]["songs"]:
+            link_list.append(entry["url"])
+        if content["response"]["next_page"] is not None:
+            content = requests.get(
+                f"https://genius.com/api/artists/{api_string}"
+                f"/songs?page={content['response']['next_page']}&per_page=20&sort=popularity"
+                f"&text_format=html%2Cmarkdown"
+            ).json()
+        else:
+            break
+    return link_list
+
+
+def _export_cloud(data_set: str) -> WordCloud:
+    """
+    Processes the string into a word cloud, returning the cloud
+    """
+    wordcloud = WordCloud(
+        width=1080,
+        height=1080,
+        background_color="black",
+        stopwords=COMBINED_STOPWORDS,
+        min_font_size=8,
+        max_words=150,
+        relative_scaling=0.7,
+    ).generate(unidecode(data_set))
+    return wordcloud
+
+
+def find_api(page: str, name: str) -> str:
+    """
+    Finds the api key for an artist on their artist page
+    """
+    response = requests.get(page)
+    candidates = re.findall(r"artists/[0-9]+", response.text)
+    api_string = ""
+    for candidate in candidates:
+        content = requests.get(f"https://genius.com/api/{candidate}").json()
+        if unidecode(re.sub(r"\W", "", name.lower())) in unidecode(
+            re.sub(r"\W", "", content["response"]["artist"]["name"].lower())
+        ):
+            api_string = re.sub(r"artists/", "", candidate)
+            return api_string
+    return api_string
+
+
+def _process_lyrics(url: str) -> str:
+    """
+    Processes the lyrics for a particular webpage and returns them as a nicely formatted string
+    This function is called by convert_lyrics as part of a multiprocessing pool
+    """
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    lyric_elements = soup.find_all("div", class_=LYRIC_CLASS)
+    portions = [
+        re.sub(CLEAN_LYRICS_RE, " ", item.decode_contents().lower())
+        for item in lyric_elements
+    ]
+    return " ".join(re.sub(r"\s+", " ", portion) for portion in portions)
+
+
+def _convert_lyrics(song_links: list[str]) -> str:
+    """
+    Processes the content of the webpage links lists into neatly formatted lyrics strings.
+    """
+    print("Processing lyrics...")
+    if len(song_links) > 250:
+        print("This may take a while...")
+    # Multiprocess lyrics
+    with Pool() as pool:
+        data_set = pool.map(_process_lyrics, song_links)
+        pool.close()
+    return " ".join(data_set)
+
+
+def cloud_hook(artist_name: str) -> WordCloud or None:
+    decode_artist = unidecode(artist_name)
+    try:
+        links = build_song_links(build_artist_page(decode_artist), decode_artist)
+        return _export_cloud(_convert_lyrics(links))
+    except ValueError:
+        return None
---
+++ cli.py
@@ -0,0 +1,58 @@
+import re
+import sys
+from artistwordcloud.cloud_creation import cloud_hook
+from artistwordcloud.constants import ARTIST_RE
+from multiprocessing import freeze_support
+from unidecode import unidecode
+from wordcloud import WordCloud
+
+
+if __name__ == "__main__":
+    freeze_support()
+    cmd_args = sys.argv[1:]
+    # Ran through double click or no CLI arguments
+    if len(cmd_args) == 0:
+        while True:
+            artist = input("Enter artist name (or press enter to exit): ")
+            output_name = ""
+            if artist == "":
+                break
+            try:
+                output_name = re.sub(
+                    ARTIST_RE, "", unidecode(artist).replace(" ", "-").lower()
+                )
+                cloud: WordCloud = cloud_hook(artist)
+                cloud.to_file(f"{output_name}.png")
+                print(f"Saved word cloud as {output_name}.png!")
+            except ValueError:
+                artist = input(
+                    f"Artist {artist} could not be found on Genius.\n"
+                    f"Please input a new artist or press enter to close the program: "
+                )
+            except OSError:
+                print(
+                    f"Could not save {(output_name + '.png') if (output_name is not None) else 'file'}\nYou may not have access to write in this directory."
+                )
+    else:
+        artists = cmd_args
+        # Create a list of lists with indices labeled by their names
+        for artist in artists:
+            print(f"\n\nCurrent artist: {artist}")
+            output_name = ""
+            try:
+                output_name = re.sub(
+                    ARTIST_RE, "", unidecode(artist).replace(" ", "-").lower()
+                )
+                cloud: WordCloud = cloud_hook(artist)
+                cloud.to_file(f"{output_name}.png")
+                print(f"Saved word cloud as {output_name}.png!")
+            except ValueError:
+                print(
+                    f"Artist {artist} could not be found on Genius. "
+                    f"Please ensure that it is spelled correctly in quotes.",
+                    file=sys.stderr,
+                )
+            except OSError:
+                print(
+                    f"Could not save {(output_name + '.png') if (output_name is not None) else 'file'}\nYou may not have access to write in this directory."
+                )
